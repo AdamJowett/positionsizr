471819429ab44084252d84a979f748a29e9f3b93
Adam Jowett <akuma.me@gmail.com>
2014-04-13 20:24:28 +1000

    added bootstrap and jquery

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var gulp = require('gulp-less');
+var gulp = require('gulp-ftp');
+var gulp = require('gulp-watch');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
