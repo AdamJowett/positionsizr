58dd7f92c7073f138211edbe0c62612e92fad8e0
Adam Jowett <akuma.me@gmail.com>
2014-04-15 22:15:19 +1000

    Moved variables out into it's own less file

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@ var gulp = require('gulp-less');
 var gulp = require('gulp-ftp');
 var gulp = require('gulp-watch');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', function () {
+	gulp.src('src/*')
+		.pipe(ftp({
+			host: 'rawincome.com',
+			user: 'adam@rawincome.com',
+			pass: 'sa00lj1010'
+		}))
+		.pipe(watch(function(files) {
+            return files.pipe(sass())
+                .pipe(gulp.dest('./dist/'));
+        }));
 });
